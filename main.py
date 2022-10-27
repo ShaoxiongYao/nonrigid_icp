6b0919a8444cd64c73d281515d82d41f2bcaf280
Shaoxiong Yao <syao16@illinois.edu>
2022-10-27 09:58:24 -0500

    add visualization

--- main.py
+++ main.py
@@ -7,38 +7,40 @@ from nricp import nonrigidIcp
 
 
 if __name__ == '__main__':
-    #read source file
-        
+    # read source file  
     sourcemesh = o3d.io.read_triangle_mesh("data/source_test.obj")
     targetmesh = o3d.io.read_triangle_mesh("data/target_half.obj")
     sourcemesh.compute_vertex_normals()
     targetmesh.compute_vertex_normals()
 
+    # visualize source and target meshes
+    sourcemesh.paint_uniform_color([0.1, 0.9, 0.1])
+    targetmesh.paint_uniform_color([0.9,0.1,0.1])
     o3d.visualization.draw_geometries([sourcemesh, targetmesh])
 
 
-
-
-    #first find rigid registration
+    # first find rigid registration
     # guess for inital transform for icp
     initial_guess = np.eye(4)
     affine_transform = icp(sourcemesh,targetmesh,initial_guess)
 
 
-    #creating a new mesh for non rigid transform estimation 
+    # creating a new mesh for non rigid transform estimation 
     refined_sourcemesh = copy.deepcopy(sourcemesh)
     refined_sourcemesh.transform(affine_transform)
     refined_sourcemesh.compute_vertex_normals()
 
+    targetmesh.paint_uniform_color([0.9,0.1,0.1])
+    refined_sourcemesh.paint_uniform_color([0.1,0.1,0.9])
+    o3d.visualization.draw_geometries([targetmesh, refined_sourcemesh])
 
-    #non rigid registration
+
+    # non rigid registration
     deformed_mesh = nonrigidIcp(refined_sourcemesh,targetmesh)
 
-
-
-sourcemesh.paint_uniform_color([0.1, 0.9, 0.1])
-targetmesh.paint_uniform_color([0.9,0.1,0.1])
-deformed_mesh.paint_uniform_color([0.1,0.1,0.9])
-o3d.visualization.draw_geometries([targetmesh,deformed_mesh])
+    sourcemesh.paint_uniform_color([0.1, 0.9, 0.1])
+    targetmesh.paint_uniform_color([0.9,0.1,0.1])
+    deformed_mesh.paint_uniform_color([0.1,0.1,0.9])
+    o3d.visualization.draw_geometries([targetmesh,deformed_mesh])
 
 
